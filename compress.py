b5059f830e493203d2dd3935e57f96d948c6ad10
xiazeyu <xiazeyu_2011@126.com>
2021-02-12 14:59:37 +0800

    intergrity test

--- compress.py
+++ compress.py
@@ -5,7 +5,7 @@ import sys
 corecount = multiprocessing.cpu_count()
 
 def compress(dirName, verbose = False):
-  command = f'7z a -r -bt -scsUTF-8 -sccUTF-8 -mx6 -mmt{corecount} "out/{dirName}.zip" "./work/{dirName}/*"'
+  command = f'7z a -r -scsUTF-8 -sccUTF-8 -mx6 -mmt{corecount} "out/{dirName}.zip" "./work/{dirName}/*"&&7z t "out/{dirName}.zip"'
   print(command)
   try:
     subprocess.run(command, shell=True, check=True)
